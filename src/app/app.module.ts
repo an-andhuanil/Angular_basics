377a4d4a37e9db31650aca2dd23fdc6f90833d8a
AnandhuAuzone <118871949+AnandhuAuzone@users.noreply.github.com>
2024-08-18 17:59:46 +0530

    App set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,37 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ShortenPipe } from './shorten.pipe';
-import { FilterPipe } from './filter.pipe';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGaurd } from './auth-gaurd.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-gaurd.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe,
-    FilterPipe
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ServersService,AuthService,AuthGaurd,CanDeactivateGuard],
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
