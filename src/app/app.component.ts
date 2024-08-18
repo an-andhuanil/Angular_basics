e59611e73087c0ca8de86025b039052d7305f858
AnandhuAuzone <118871949+AnandhuAuzone@users.noreply.github.com>
2024-08-18 18:20:08 +0530

    App set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,13 @@
 import { Component } from '@angular/core';
 
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
-export class AppComponent {
-  serverElements = [{type:'server', name:'TestServer', content:'abcdefghijklm'}];
-
-  onServerAdded(serverData :{servername:string, serverContent: string}){
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.servername,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(blueprintData :{servername:string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.servername,
-      content: blueprintData.serverContent
-    });
-  }
-
-
-
-  onChangeElement(){
-    this.serverElements[0].name = 'changed!';
-  }
-
-  oddNumbers : number[] = [];
-  evenNumbers : number[] = [];
-  onIntervalFired(firedNumer : number){
-    if(firedNumer % 2 === 0){
-      this.evenNumbers.push(firedNumer);
-    }else{
-      this.oddNumbers.push(firedNumer);
-    }
-
-  }
-
-  
-
-}
+export class AppComponent {}
